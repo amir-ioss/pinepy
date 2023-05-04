2017988a5881d103167193807b58d6cb1d12359e
amir_abbasy <87001985+amir-ioss@users.noreply.github.com>
2023-05-04 08:13:00 +0530

    5/4/23

--- ami.py
+++ ami.py
@@ -18,7 +18,7 @@ import talib
 import json
 import pandas as pd
 import numpy as np
-from _help.log import p 
+# from _help.log import p 
 from data.data_test import test_data 
 from data.dates import dates 
 from data.coins import coins
@@ -34,15 +34,12 @@ def percentage(num1, num2):
 def tpsl_price(num, per = 0.1):
     return {"tp":num+price_percentage(num, per), "sl":num-price_percentage(num, per)}
 
-
-# TEST 
-
-def PIVOTHIGH(high: np.ndarray, left:int, right: int):
+def pivothigh(high: np.ndarray, left:int, right: int):
     pivots = np.roll(talib.MAX(high, left + 1 + right), -right)
     pivots[pivots != high] = np.NaN
     return pivots
 
-def PIVOTLOW(low: np.ndarray, left:int, right: int):
+def pivotlow(low: np.ndarray, left:int, right: int):
     pivots = np.roll(talib.MIN(low, left + 1 + right), -right)
     pivots[pivots != low] = np.NaN
     return pivots
@@ -58,7 +55,7 @@ class Boat:
 
     def run(self,):
         while True:
-            p("\n                         ---- scanning ({0})  ----\n".format(len(self.coins)))
+            print("\n                         ---- scanning ({0})  ----\n".format(len(self.coins)))
             this_minute = datetime.today().minute
             abs_num = this_minute/5
             
@@ -82,13 +79,13 @@ class Boat:
                 now = datetime.utcnow()
                 unixtime = calendar.timegm(now.utctimetuple())
                 since = (unixtime - 60*60) * 1000 # UTC timestamp in milliseconds
-                # start_dt = datetime.fromtimestamp(ohlcv[0][0]/1000)
-                date_test = self.dates[idx] #self.dates[idx] # "2022-12-29 00:00:00"
-                ohlcv_ = Ohlcv("XRP/USDT", interval, None, 300)
-                # ohlcv_ = Ohlcv(self.coins[idx]['symbol'], interval, date_test, 300)
-                kandles = Hkac(ohlcv_.ohlcv).kandles
+                # # start_dt = datetime.fromtimestamp(ohlcv[0][0]/1000)
+                # date_test = self.dates[idx] #self.dates[idx] # "2022-12-29 00:00:00"
+                # ohlcv_ = Ohlcv("XRP/USDT", interval, None, 300)
+                # # ohlcv_ = Ohlcv(self.coins[idx]['symbol'], interval, date_test, 300)
+                # kandles = Hkac(ohlcv_.ohlcv).kandles
+                kandles = Hkac(test_data).kandles # TEST DATA
                 # p(kandles)
-                # kandles = test_data # test data
 
                 df = pd.DataFrame(kandles, columns = ['Time', 'Open', 'High', 'Low', 'Close', 'Volume', 'HKOpen', 'HKHigh', 'HKLow', 'HKClose'])
                 # indicators
@@ -106,8 +103,8 @@ class Boat:
 
                 LEFT_BAR = 21
                 RIGHT_BAR = 21
-                hhs = PIVOTHIGH(close, LEFT_BAR, RIGHT_BAR)
-                lls = PIVOTLOW(close, LEFT_BAR, RIGHT_BAR)
+                hhs = pivothigh(close, LEFT_BAR, RIGHT_BAR)
+                lls = pivotlow(close, LEFT_BAR, RIGHT_BAR)
 
                 isOrderPlaced = False
                 
@@ -119,12 +116,10 @@ class Boat:
                 LEVERAGE = 20
 
                 # EXPLORING
-
                 hh = [0,0]
                 ll = [0,0]
-                # hh_price = 0
-                # hh_time = "00:00:00 00:00:00"
-                # ll_price = 0
+                farFrom = 0
+                breakout =  False
 
                 for index in range(len(df['Close'])):
                     time =  datetime.fromtimestamp(np.nan_to_num(df['Time'].to_numpy()[index])/1000)
@@ -134,22 +129,24 @@ class Boat:
                     if(not np.isnan(lls[index]) and ll[-1] != lls[index]):
                         print(time,ll[-1], "last----POINT-------new", lls[index])
                         ll.append(lls[index])
+                        farFrom = 0
+                    farFrom += 1
 
-                    # print(time, percentage(ll[-1], close[index]))
-                    # if(close[index] > hh_price):
-                    #     # hh[-1]['price']  = close[index]
-                    #     # hh[-1]['time']  = _time4
-                    #     hh_price = close[index]
-                    #     hh_time = time
 
-                    last_ll_p = percentage(ll[-2], _open[index])
-                    if(last_ll_p < .05 and not last_ll_p < 0 and not isOrderPlaced):
+                    # ENTRY ////////////////////////////////////////////////////////
+                    last_ll_p = percentage(ll[-1], _open[index])
+                    entry_common = not isOrderPlaced and farFrom > RIGHT_BAR
+                    entry_height = last_ll_p < .05 and not last_ll_p < 0
+
+                    if(entry_height and entry_common):
                         print(time,"ENTRY", _open[index])
-                        print("last point -", ll[-2])
+                        print("last point -", ll[-1])
                         isOrderPlaced = True
                         entryPrice = _open[index]
                         pass
                     
+
+                    # EXIT /////////////////////////////////////////////////////////
                     p_change = percentage(entryPrice, _open[index])
                     if((p_change > 0.40 or -.1 > p_change) and isOrderPlaced):
                         print(time,"EXIT", _open[index], p_change,'\n')
@@ -173,7 +170,7 @@ class Boat:
                         trades.append(pl)
                         pass
 
-
+               # RESULTS //////////////////////////////////////////////////////////////////
                 print(self.dates[idx], sum(trades), trades)
                 self.month.append(sum(trades))
 
@@ -199,29 +196,3 @@ bot.run()
 # loop.run_until_complete(bot.run())
 
 # loop.close()
-
-# UTILS
-# def percentage(num1, num2):
-#     return 100-((num1/num2)*100)
-# print(percentage(0.3366, 0.3369))
-
-# Trailing test
-# changes = [0.08, 0.04, 0.12, 0.18, 0.22 ,0.31, 0.35,  0.32, 0.38, 0.41, 0.39]
-# min_trailing = 0.2
-# trailing = 0.2
-# enableTrailing = False
-# for ch in changes:
-#     print("->", ch, trailing)
-#     if(ch > trailing):
-#         trailing += 0.1
-#         enableTrailing = True
-
-#     if(ch < trailing-0.1 and enableTrailing):
-#         print("leave")
-#    p_change = percentage(entryPrice, close[index-1]) 
-#                     trailing = 0.2
-#                     if(p_change > trailing+0.1)
-#                         trailing += trailing+0.1
-
-
-# print(tpsl_price(1000, 2)['tp'], tpsl_price(1000, 1)['sl'])
